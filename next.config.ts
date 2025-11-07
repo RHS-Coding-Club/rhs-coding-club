f8c866ee0d2b8283b4cf8ec439ee3d77709b048a
JashanMaan28 <166474619+JashanMaan28@users.noreply.github.com>
2025-11-06 20:45:16 -0800

    fix: update security headers to allow tweakcn

--- next.config.ts
+++ next.config.ts
@@ -33,8 +33,8 @@ const nextConfig: NextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'X-Frame-Options',
-            value: 'DENY',
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://tweakcn.com;",
           },
           {
             key: 'X-Content-Type-Options',
